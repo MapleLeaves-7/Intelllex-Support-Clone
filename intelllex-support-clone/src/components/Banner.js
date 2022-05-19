4bf70ead2a8cb9caaf71ed147ecfccb5f8715de7
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-05-19 16:13:23 +0800

    Style banner halfway

--- intelllex-support-clone/src/components/Banner.js
+++ intelllex-support-clone/src/components/Banner.js
@@ -4,16 +4,20 @@ import { ReactComponent as NavSquareArrow } from '../images/nav-square-arrow.svg
 const Banner = () => {
   return (
     <div id="banner">
-      <img src={logo} alt="Intelllex logo" width="50px" height="50px" />
-      <h1>Intelllex</h1>
-      <h2>| Support </h2>
-      <div className="nav-to-intelllex">
-        <NavSquareArrow
-          className="nav-square-arrow"
-          width="50px"
-          height="50px"
-        />
-        Back to INTELLLEX
+      <div className="main-banner">
+        <div className="logo-container">
+          <img src={logo} alt="Intelllex logo" className="logo" />
+          <h1 className="heading">Intelllex</h1>
+        </div>
+        <h2 className="subheading">Support</h2>
+        <div className="nav-to-intelllex">
+          <NavSquareArrow
+            className="nav-square-arrow"
+            width="50px"
+            height="50px"
+          />
+          Back to INTELLLEX
+        </div>
       </div>
       <div className="caption">Get more out of INTELLLEX</div>
     </div>
