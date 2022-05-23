bbd3e9418d706c61978029313f8ae5d284e25bf8
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-05-23 13:19:56 +0800

    Subpage skeleton design

--- intelllex-support-clone/src/components/SubPage.js
+++ intelllex-support-clone/src/components/SubPage.js
@@ -1,5 +1,37 @@
 import { Banner } from '../components';
+import { Navigation } from './Navigation';
+import { Link } from 'react-router-dom';
 
 export function SubPage() {
-  return <Banner main={false} />;
+  let sections = [
+    { id: 'legal-commentaries', heading: 'Legal Commentaries' },
+    { id: 'cases', heading: 'Cases' },
+  ];
+  let title = 'SOURCE+ Coverage';
+  let updatedDate = { day: '14', month: 'April', year: '2022' };
+  let content = (
+    <div>
+      Testing this out 123 <div>testing this some more</div>
+    </div>
+  );
+  return (
+    <>
+      <Banner main={false} />
+      <div className="breadcrumb">
+        <Link to="/">Support</Link>
+        <span>&gt;</span>
+        <span>{title}</span>
+      </div>
+      <div className="subpage-main-section">
+        <Navigation sections={sections} isSubPage={true} />
+        <div className="subpage-main-content">
+          <h1>{title}</h1>
+          <div>
+            Updated on {updatedDate.month} {updatedDate.day}, {updatedDate.year}
+          </div>
+          {content}
+        </div>
+      </div>
+    </>
+  );
 }
