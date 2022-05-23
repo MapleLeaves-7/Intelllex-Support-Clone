41d50836fd2f64e5d20054653de5871a230a35e1
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-05-23 18:15:17 +0800

    Complete style using tailwind

--- intelllex-support-clone/src/components/Banner.js
+++ intelllex-support-clone/src/components/Banner.js
@@ -3,19 +3,41 @@ import { ReactComponent as NavSquareArrow } from '../images/nav-square-arrow.svg
 
 export function Banner() {
   return (
-    <div>
-      <div>
-        <div>
-          <img src={logo} alt="Intelllex logo" />
-          <h1>Intelllex</h1>
+    <div
+      style={{
+        background: 'linear-gradient(to right,#2a4072 0, #4c78db)',
+      }}
+      className="pt-10 px-20 pb-9 text-white"
+    >
+      <div className="flex items-center font-banner mb-10">
+        <div className="flex items-center">
+          <img className="w-12 h-12 mr-4" src={logo} alt="Intelllex logo" />
+          <h1
+            style={{ letterSpacing: '0.4rem' }}
+            className="font-banner uppercase text-3xl pr-6"
+          >
+            Intelllex
+          </h1>
         </div>
-        <h2>Support</h2>
-        <div>
-          <NavSquareArrow />
-          <a href="https://intelllex.com/">Back to INTELLLEX</a>
+        <h2
+          style={{ borderLeft: '3px solid', fontSize: '1.68rem' }}
+          className="inline-block font-banner text-yellow-400 uppercase tracking-widest font-bold pl-6"
+        >
+          Support
+        </h2>
+        <div className="flex items-center ml-auto">
+          <NavSquareArrow className="w-6 h-6 mr-2" />
+          <a
+            className="font-primary text-lg font-bold"
+            href="https://intelllex.com/"
+          >
+            Back to INTELLLEX
+          </a>
         </div>
       </div>
-      <div>Get more out of INTELLLEX</div>
+      <div style={{ fontSize: '3.125rem' }} className="font-primary mb-6">
+        Get more out of INTELLLEX
+      </div>
     </div>
   );
 }
