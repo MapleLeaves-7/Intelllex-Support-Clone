7cffdc79ff13f926ac74f0c6f7657df3b12402a7
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-05-24 18:31:03 +0800

    Use tailwind directly and add media queries

--- intelllex-support-clone/src/components/Navigation.js
+++ intelllex-support-clone/src/components/Navigation.js
@@ -7,7 +7,8 @@ export function Navigation({ sections }) {
         <div
           key={section.id}
           onClick={navService.scrollToID(section.id)}
-          className="nav-section-item"
+          className="px-4 py-2 mb-2 font-primary font-medium tracking-tight leading-tight text-lg text-gray-600 rounded-sm cursor-pointer 
+          hover:bg-slate-300 hover:bg-opacity-30"
         >
           {section.heading}
         </div>
