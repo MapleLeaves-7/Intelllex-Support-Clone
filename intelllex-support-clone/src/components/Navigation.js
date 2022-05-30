c5715643a6cb2a88a6fc1d32b03e8ecbad72b557
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-05-30 11:05:03 +0800

    Use CSS to smooth scroll element into view

--- intelllex-support-clone/src/components/Navigation.js
+++ intelllex-support-clone/src/components/Navigation.js
@@ -1,17 +1,15 @@
-import navService from '../services.js/nav';
-
 export function Navigation({ sections }) {
   return (
     <div>
       {sections.map(section => (
-        <div
+        <a
           key={section.id}
-          onClick={navService.scrollToID(section.id)}
-          className="px-4 py-2 mb-2 font-primary font-medium tracking-tight leading-tight text-lg text-gray-600 rounded-sm cursor-pointer 
+          href={`#${section.id}`}
+          className="block px-4 py-2 mb-2 font-primary font-medium tracking-tight leading-tight text-lg text-gray-600 rounded-sm cursor-pointer 
           hover:bg-slate-300 hover:bg-opacity-30"
         >
           {section.heading}
-        </div>
+        </a>
       ))}
     </div>
   );
