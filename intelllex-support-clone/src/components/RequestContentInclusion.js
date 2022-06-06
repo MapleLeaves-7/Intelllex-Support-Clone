5e70e23e7c811a98024799ddb6d60a1225bf501b
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-06-06 18:07:10 +0800

    feat: create request content inclusion component

---
+++ intelllex-support-clone/src/components/RequestContentInclusion.js
@@ -0,0 +1,16 @@
+export function RequestContentInclusion() {
+  return (
+    <div className="pt-6 pb-4 font-content font-bold italic text-dark-gray">
+      <p>
+        Let us know what content you would like us to include in SOURCE+ by
+        emailing us at{' '}
+        <a
+          href="mailto:support@intelllex.com"
+          className="text-blue-700 underline active:text-red-500"
+        >
+          support@intelllex.com
+        </a>
+      </p>
+    </div>
+  );
+}
