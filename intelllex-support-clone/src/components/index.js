9214ecbe2c06c6729efb55781a759f758f79fcb1
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-06-06 18:08:00 +0800

    chore: update exports
    include breadcrumb and requestContentInclusion

--- intelllex-support-clone/src/components/index.js
+++ intelllex-support-clone/src/components/index.js
@@ -2,3 +2,5 @@ export * from './Banner';
 export * from './Footer';
 export * from './MainContentItem';
 export * from './Navigation';
+export * from './Breadcrumb';
+export * from './RequestContentInclusion';
