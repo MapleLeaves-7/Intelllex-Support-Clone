4d7d307e21f6a3f2b8d4361510fd74688e3a54dc
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-06-06 18:00:11 +0800

    feat: show section title if subpage and change hover color

--- intelllex-support-clone/src/components/Navigation.js
+++ intelllex-support-clone/src/components/Navigation.js
@@ -1,12 +1,17 @@
-export function Navigation({ sections }) {
+export function Navigation({ sections, isSubPage }) {
   return (
-    <div>
+    <div className="pr-4">
+      {isSubPage && (
+        <h3 className="pb-4 font-primary font-bold text-2xl text-dark-gray">
+          Sections:
+        </h3>
+      )}
       {sections.map(section => (
         <a
           key={section.id}
           href={`#${section.id}`}
-          className="block px-4 py-2 mb-2 font-primary font-medium tracking-tight leading-tight text-lg text-gray-600 rounded-sm cursor-pointer 
-          hover:bg-slate-300 hover:bg-opacity-30"
+          className="block px-4 py-2 mb-2 font-primary font-medium tracking-tight leading-tight text-lg text-gray-600 rounded-md cursor-pointer 
+          hover:bg-hover-color"
         >
           {section.heading}
         </a>
