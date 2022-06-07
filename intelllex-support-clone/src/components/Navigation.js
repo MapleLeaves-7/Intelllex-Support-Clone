d3612dca6c3ea81bd4f552ec5649e1aa5e78ac05
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-06-07 18:39:02 +0800

    refactor: use function to generate id
    account for differently formatted data for main page

--- intelllex-support-clone/src/components/Navigation.js
+++ intelllex-support-clone/src/components/Navigation.js
@@ -1,3 +1,5 @@
+import { convertToId } from '../utils';
+
 export function Navigation({ sections, isSubPage }) {
   return (
     <div className="pr-4">
@@ -6,16 +8,25 @@ export function Navigation({ sections, isSubPage }) {
           Sections:
         </h3>
       )}
-      {sections.map(section => (
-        <a
-          key={section.id}
-          href={`#${section.id}`}
-          className="block px-4 py-2 mb-2 font-primary font-medium tracking-tight leading-tight text-lg text-gray-600 rounded-md cursor-pointer 
+      {sections.map(section => {
+        let id;
+        if (section.hasOwnProperty('id')) {
+          id = section.id;
+          section = section.heading;
+        } else {
+          id = convertToId(section);
+        }
+        return (
+          <a
+            key={id}
+            href={`#${id}`}
+            className="block px-4 py-2 mb-2 font-primary font-medium tracking-tight leading-tight text-lg text-gray-600 rounded-md cursor-pointer 
           hover:bg-hover-color"
-        >
-          {section.heading}
-        </a>
-      ))}
+          >
+            {section}
+          </a>
+        );
+      })}
     </div>
   );
 }
