2d3c73e2ac13f22a697d4d23d3db10bdfe77f0d6
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-06-08 18:47:39 +0800

    fix: let breadcrumb remain in view on small screen

--- intelllex-support-clone/src/components/Breadcrumb.js
+++ intelllex-support-clone/src/components/Breadcrumb.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 export function Breadcrumb({ pageName }) {
   return (
-    <div className="-ml-4 font-primary">
+    <div className="md:-ml-4 font-primary">
       <Link to="/" className="text-blue-600">
         Support
       </Link>
