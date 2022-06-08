d67506deb2bca0c00f713c558b118432998b3516
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-06-08 15:49:34 +0800

    feat: add route for search in stacks

--- intelllex-support-clone/src/pages/stacks/Stacks.js
+++ intelllex-support-clone/src/pages/stacks/Stacks.js
@@ -3,6 +3,7 @@ import { Banner } from '../../components';
 import { Error } from '../Error';
 import { SyncFoldersFiles } from './SyncFoldersFiles';
 import { OrganiseStacks } from './OrganiseStacks';
+import { SearchInStacks } from './SearchInStacks';
 export function Stacks() {
   let { page } = useParams();
   let content;
@@ -12,6 +13,9 @@ export function Stacks() {
       break;
     case 'organise-your-stacks':
       content = <OrganiseStacks />;
+      break;
+    case 'search-in-stacks':
+      content = <SearchInStacks />;
       break;
     default:
       content = <Error />;
