9358f7aabcac28c91ca3b066eb218acb3da02cda
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-06-08 15:25:48 +0800

    feat: add route for organise stacks

--- intelllex-support-clone/src/pages/stacks/Stacks.js
+++ intelllex-support-clone/src/pages/stacks/Stacks.js
@@ -2,12 +2,16 @@ import { useParams } from 'react-router-dom';
 import { Banner } from '../../components';
 import { Error } from '../Error';
 import { SyncFoldersFiles } from './SyncFoldersFiles';
+import { OrganiseStacks } from './OrganiseStacks';
 export function Stacks() {
   let { page } = useParams();
   let content;
   switch (page) {
     case 'sync-or-upload-your-folders-and-files':
       content = <SyncFoldersFiles />;
+      break;
+    case 'organise-your-stacks':
+      content = <OrganiseStacks />;
       break;
     default:
       content = <Error />;
