563af223101b2a029e411aab9e6d77874dd4f101
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-06-08 18:02:48 +0800

    chore: allow custom caption

--- intelllex-support-clone/src/components/RequestContentInclusion.js
+++ intelllex-support-clone/src/components/RequestContentInclusion.js
@@ -1,9 +1,8 @@
-export function RequestContentInclusion() {
+export function RequestContentInclusion({ caption }) {
   return (
     <div className="pt-6 pb-4 font-content font-bold italic text-dark-gray">
       <p>
-        Let us know what content you would like us to include in SOURCE+ by
-        emailing us at{' '}
+        {caption}
         <a
           href="mailto:support@intelllex.com"
           className="text-blue-700 underline active:text-red-500"
