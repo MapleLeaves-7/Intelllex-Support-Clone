d24358538c90371bea811e9967e451e0c7bae547
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-06-08 18:03:36 +0800

    feat: link to refine your search page

--- intelllex-support-clone/src/pages/source/Source.js
+++ intelllex-support-clone/src/pages/source/Source.js
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom';
 import { Banner } from '../../components';
 import { Error } from '../Error';
+import { RefineYourSearch } from './RefineYourSearch';
 import { SourceCoverage } from './SourceCoverage';
 export function Source() {
   let { page } = useParams();
@@ -8,6 +9,9 @@ export function Source() {
   switch (page) {
     case 'source-coverage':
       content = <SourceCoverage />;
+      break;
+    case 'refine-your-search':
+      content = <RefineYourSearch />;
       break;
     default:
       content = <Error />;
