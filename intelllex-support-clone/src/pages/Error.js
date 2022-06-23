f01cc426f49a8e7e800e8064bb6680258895b3f6
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-06-23 17:34:36 +0800

    chore: style error page

--- intelllex-support-clone/src/pages/Error.js
+++ intelllex-support-clone/src/pages/Error.js
@@ -1,3 +1,17 @@
+import { Banner } from '../components';
 export function Error() {
-  return <div>404 Page Not Found</div>;
+  return (
+    <>
+      <Banner isMainPage={true} />
+      <main className="flex justify-center mb-14">
+        <div className="flex flex-col md:flex-row gap-6 pt-16 md:pt-20 container-width-breakpoints">
+          <div className="flex flex-col">
+            <h1 className="font-primary font-bold text-3xl">
+              404 Page Not Found
+            </h1>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
